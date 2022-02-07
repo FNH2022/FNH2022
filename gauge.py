16e707a064e38cd73dd0b68d6e6a150bec88058d
FNH2022 <98783960+FNH2022@users.noreply.github.com>
2022-02-07 10:57:52 -0800

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -1,3 +1,12 @@
+import argparse
+import cv2
+import os
+
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
 import PIL
 
 from PIL import Image
